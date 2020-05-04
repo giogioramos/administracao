5a85ec97f6e4bf888e3684632f240fd628eb543d
Tokengi <tokengi@gmail.com>
2020-05-04 03:02:09 -0300

    mudanças SQL

--- gerar_tabelas.sql
+++ gerar_tabelas.sql
@@ -4,11 +4,25 @@ CREATE TABLE IF NOT EXISTS administracao.cliente (
 id int NOT NULL AUTO_INCREMENT,
 PRIMARY KEY (id), 
 nome VARCHAR(150) NOT NULL, 
-datanascimento DATE NOT NULL,
+data_nascimento DATE NOT NULL,
 cpf VARCHAR(11) NOT NULL, 
 rg VARCHAR(20) NOT NULL,
 telefone VARCHAR(10) NOT NULL) 
 CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS administracao.endereco ( 
+id int NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (id), 
+id_cliente int NOT NULL,
+rua VARCHAR(150) NOT NULL,
+numero VARCHAR(5) NOT NULL, 
+bairro VARCHAR(150) NOT NULL, 
+complemento VARCHAR(150) NOT NULL, 
+cep int NOT NULL, 
+cidade VARCHAR(150) NOT NULL, 
+estado VARCHAR(2) NOT NULL) 
+CHARSET=utf8 COLLATE utf8_unicode_ci;
+
 
 CREATE TABLE IF NOT EXISTS administracao.usuario (
 id int NOT NULL AUTO_INCREMENT,
